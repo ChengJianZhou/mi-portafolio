df69a4cd54c032e6429c6c2bf9be600f5a953313
Marcos Zhou <marcoszhou98@gmail.com>
2026-04-12 03:25:02 +0200

    first test on production: bug fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import { AboutHero }     from '../components/About/AboutHero'
 import { AboutTimeline } from '../components/About/AboutTimeline'
 import { AboutSkills }   from '../components/About/AboutSkills'
 import { AboutPersonal } from '../components/About/AboutPersonal'
-import '../styles/About.css'
+import '../styles/about.css'
 
 export default function AboutPage() {
   return (
